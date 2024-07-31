e0ffced70a8c12e71969fecd6dbb7ee586354bdd
FookNaRak <66070098@kmitl.ac.th>
2024-07-31 16:19:25 +0700

    Easy_Loop

--- Easy_Loop.c
+++ Easy_Loop.c
@@ -3,8 +3,18 @@ int main()
 {
     int number;
     scanf("%d", &number);
-    for(int i=number; i==0; i--){
-        printf("%d", i);
+    if(number > 0){
+        for(int i=number; i>=0;i--){
+            printf("%d ", i);
+        }
+    }
+    else if(number == 0){
+        printf("%c",'0');
+    }
+    else{
+        for(int i=number; i<=0; i++){
+            printf("%d ", i);
+        }
     }
     return 0;
 }
