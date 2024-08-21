e2ec3425b958a88660142d9f92317b9e77873535
FookNaRak <66070098@kmitl.ac.th>
2024-08-21 15:18:08 +0700

    Blood Pressure

---
+++ Blood_Pressure.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int sbp, dbp;
+
+    scanf("%d", &sbp);
+    scanf("%d", &dbp);
+    if(sbp>140 || dbp>90){
+        printf("Hypertension");
+    }
+    else{
+        printf("Normal");
+    }
+    return 0;
+}
